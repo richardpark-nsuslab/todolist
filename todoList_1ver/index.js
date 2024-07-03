cc62cf715b1f0da12681dc16b8c9200ccc4125d8
richardpark-nsuslab <139844906+richard-park-nsuslab@users.noreply.github.com>
2024-07-03 12:14:00 +0900

    Add files via upload
    바닐라 자바스크립트 버전

--- todoList_1ver/index.js
+++ todoList_1ver/index.js
@@ -1,90 +1,72 @@
-var list =  // HTML을 짧게 만들기 위해 변수로 넣었습니다.
-`<div class="card">
-  <div class="card-body">
-    <p class="card-num"><small></small></p>
-    <h5 class="card-title"></h5>
-    <p class="card-text"></p>
-    <button type="button" class="btn btn-danger delete-btn">삭제</button>
-  </div>
-</div>`
+// 첫번째 관문
+// 1. 세이브 누를때 공백체크
+// 2. 공백시 non-text 클래스 추가
+// 3. 추가 후 5초 뒤 클래스 삭제
+//======================================
+
+//두번째 관문
+//1. 인풋 내용을 스토리지에 넣기
+
+//세번째 관문
+//1. 클리어 버튼 클릭시
+//2. .card_list의 html 클리어
+//3. 스토리지 클리어
+
+//네번째 관문
+//1. 로딩시 스토리지에 데이터 불러오기
+//2. 카드리스트에 내용 넣기
 
 
+document.querySelector('#save').addEventListener('click', function(){
 
-$(document).ready(function(){
-    var todoNum = localStorage.length;
-
-    // 시작하자마자 로컬스토리지에 개수 체크 후 버튼 출력 유무 조정    
-    if(localStorage.length > 0){
-        $('#clear').show();
-    } else {
-        $('#clear').hide();
-    }
-
-    //시작하자마자 로컬스토리지에 있는 내용들을 불러오기
-    //key값의 순서대로 정렬 시키기
-
-    var localTodoCard = JSON.parse(todoNum)
-    localStorage.getItem(todoNum, localTodoCard);
-    console.log(localStorage.getItem(todoNum, localTodoCard));
-
-
-});
-
-$('#save').click(function(){
-    var todoNum = localStorage.length;
-    var todoTitle = $('#todoTitle').val();
-    var todoText = $('#todoText').val();
-
-    //저장버튼 누를때마다 공백체크
-    if(todoTitle == ''){
-        $('#todoTitle').addClass('non-text');
-
+    if(document.querySelector('#todoTitle').value == ''){
+        document.querySelector('#todoTitle').classList.add('non-text');
+        
         setTimeout(function(){
-            $('#todoTitle').removeClass('non-text');
-        }, 500)
+            document.querySelector('#todoTitle').classList.remove('non-text');
+        }, 500);
+    } else if(document.querySelector('#todoText').value == ''){
+        document.querySelector('#todoText').classList.add('non-text');
         
-    } else if(todoText == '') {
-        $('#todoText').addClass('non-text');
-
         setTimeout(function(){
-            $('#todoText').removeClass('non-text');
-        }, 500)
-    } else {
-        $('.card_list').prepend(list);
-        
-        todoNum++
-        // 입력한 내용들 html에 출력 -> 스토리지에서 값을 저장 한 뒤 그걸 출력하는걸로 바꿔야함.
-        $('.card-num').html(todoNum);
-        $('.card-title').html(todoTitle);
-        $('.card-text').html(todoText);
-        
+            document.querySelector('#todoText').classList.remove('non-text');
+        }, 500);
+    } else{
+        var todoTitle = document.querySelector('#todoTitle').value;
+        var todoText = document.querySelector('#todoText').value;
+
+        var todoNum = localStorage.length;
+        todoNum++;
+
+        var list =  // HTML을 짧게 만들기 위해 변수로 넣었습니다.
+        `<div class="card">
+          <div class="card-body">
+            <p class="card-num"><small>${todoNum}</small></p>
+            <h5 class="card-title">${document.querySelector('#todoTitle').value}</h5>
+            <p class="card-text">${document.querySelector('#todoText').value}</p>
+            <button type="button" class="btn btn-danger delete-btn">삭제</button>
+          </div>
+        </div>`
+
+
+        document.querySelector('.card_list').insertAdjacentHTML('afterbegin',list) // 최근에 만든걸 상단에 올려줌
+
         var todoCard = [todoTitle, todoText] // 스토리지에 값들 저장 한걸 변수로
         var localTodoCard = JSON.stringify(todoCard)//  스토리지의 값들이 안깨지도록 제이슨으로 저장
 
         localStorage.setItem(todoNum, localTodoCard);
-        
-    }
-
-    //저장버튼 누를때마다 로컬스토리지 체크
-    if(localStorage.length > 0){
-        $('#clear').show();
-    } else {
-        $('#clear').hide();
-    }
-
-});
-
-$('#clear').click(function(){
-
-    var clearCard = confirm('gdgd')
-
-    if(clearCard == true){
-        localStorage.clear();
-        $('.card_list').empty();
-    } else {
 
     }
-
-
 })
 
+document.querySelector('#clear').addEventListener('click', function(){
+        var clearCard = confirm('삭제 하시나요?')
+
+
+        if(clearCard == true){
+            localStorage.clear();
+            document.querySelector('.card_list').innerHTML='';
+        } else {
+
+        }
+})
