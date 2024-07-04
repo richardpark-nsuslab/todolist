4eef15ced91e5c01e257d4e9c34d6aa499e09414
richardpark-nsuslab <139844906+richard-park-nsuslab@users.noreply.github.com>
2024-07-04 14:57:56 +0900

    세번째 커밋
    저장후 폼 클리어
    각각 삭제기능 추가
    각각 수정기능 추가

--- todoList_1ver/index.js
+++ todoList_1ver/index.js
@@ -1,20 +1,8 @@
-// 첫번째 관문
-// 1. 세이브 누를때 공백체크
-// 2. 공백시 non-text 클래스 추가
-// 3. 추가 후 5초 뒤 클래스 삭제
-//======================================
+// 1. 저장 후 폼 클리어 okey
+// 2. 수정/삭제 기능 추가 
+// 3. 로컬스토리지 불러오기 okey
 
-//두번째 관문
-//1. 인풋 내용을 스토리지에 넣기
 
-//세번째 관문
-//1. 클리어 버튼 클릭시
-//2. .card_list의 html 클리어
-//3. 스토리지 클리어
-
-//네번째 관문
-//1. 로딩시 스토리지에 데이터 불러오기
-//2. 카드리스트에 내용 넣기
 
 
 document.querySelector('#save').addEventListener('click', function(){
@@ -39,12 +27,13 @@ document.querySelector('#save').addEventListener('click', function(){
         todoNum++;
 
         var list =  // HTML을 짧게 만들기 위해 변수로 넣었습니다.
-        `<div class="card">
+        `<div class="card" id="${todoNum}">
           <div class="card-body">
             <p class="card-num"><small>${todoNum}</small></p>
-            <h5 class="card-title">${document.querySelector('#todoTitle').value}</h5>
-            <p class="card-text">${document.querySelector('#todoText').value}</p>
-            <button type="button" class="btn btn-danger delete-btn">삭제</button>
+            <input value="${todoTitle}" class="card-title">
+            <input value="${todoText}" class="card-text">
+            <button type="button" class="btn btn-primary edit-btn" id="edit">수정</button>
+            <button type="button" class="btn btn-danger delete-btn" id="delete">삭제</button>
           </div>
         </div>`
 
@@ -55,9 +44,103 @@ document.querySelector('#save').addEventListener('click', function(){
         var localTodoCard = JSON.stringify(todoCard)//  스토리지의 값들이 안깨지도록 제이슨으로 저장
 
         localStorage.setItem(todoNum, localTodoCard);
+        document.querySelector('#todoTitle').value = null;
+        document.querySelector('#todoText').value = null;
+
+
 
     }
-})
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    let keys = [];
+
+
+
+    for(let i = 0; i < localStorage.length; i++){
+        keys.push(localStorage.key(i)); // i번째 배열 추가
+    }
+
+    keys.sort((a, b) => parseInt(a) - parseInt(b));
+    
+    keys.forEach(key => { // Arr을 배열을 정렬 하기 위해 forEach 사용
+        const value = JSON.parse(localStorage.getItem(key));
+
+        
+        var list =  // HTML을 짧게 만들기 위해 변수로 넣었습니다.
+        `<div class="card" id="${key}">
+        <div class="card-body">
+        <p class="card-num"><small>${key}</small></p>
+        <input value="${value[0]}" class="card-title">
+        <input value="${value[1]}" class="card-text">
+        <button type="button" class="btn btn-primary edit-btn" id="edit">수정</button>
+        <button type="button" class="btn btn-danger delete-btn" id="delete">삭제</button>
+        </div>
+        </div>`
+        
+        document.querySelector('.card_list').insertAdjacentHTML('afterbegin',list);
+        // console.log(`${key}: ${value}`);
+        
+    });
+    
+
+
+
+
+
+
+});
+
+
+document.addEventListener('click', function(event) {
+    // 클릭된 요소의 부모의 부모 요소가 존재할 경우, 그 id를 가져옴
+    var parentElement = event.target.parentElement;
+    var grandParentElement = parentElement ? parentElement.parentElement : null;
+    var grandParentElementId = grandParentElement ? grandParentElement.id : null;
+
+    if (grandParentElementId) {
+        if(event.target.id == 'delete'){
+
+            console.log(localStorage.getItem(grandParentElementId));
+            
+            var clearCard = confirm('삭제 하시나요?')
+    
+            if(clearCard == true){
+                localStorage.removeItem(grandParentElementId);
+                grandParentElement.remove();
+            }
+        } else if (event.target.id == 'edit'){
+            var clearCard = confirm('수정 하시나요?')
+
+            if(clearCard == true){
+
+
+                var cardNum = document.querySelector('.card-num small').textContent; 
+                var cardTitle = document.querySelector('.card-title').value; 
+                var cardText = document.querySelector('.card-text').value; // 카드 인풋에 있는걸 변수로
+
+                var editCard = [cardTitle, cardText] // 스토리지에 값들 저장 한걸 변수로
+                var localEditCard = JSON.stringify(editCard)//  스토리지의 값들이 안깨지도록 제이슨으로 저장
+
+                localStorage.setItem(cardNum, localEditCard);
+
+                
+
+
+                console.log(cardNum, cardTitle, cardText)
+
+
+
+            }
+        }
+        
+
+    }
+
+
+});
+
 
 document.querySelector('#clear').addEventListener('click', function(){
         var clearCard = confirm('삭제 하시나요?')
@@ -69,4 +152,4 @@ document.querySelector('#clear').addEventListener('click', function(){
         } else {
 
         }
-})
+});
